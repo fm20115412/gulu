5fee3ea23043d97017d739de3bd9749aca4787d3
fengmin <fengmin01@baidu.com>
2018-10-15 20:41:04 +0800

    icon可以放在左或右

--- src/button.vue
+++ src/button.vue
@@ -1,14 +1,17 @@
 <template>
-    <button class='v-button'>
+    <button class='v-button' :class='{}'>
          <svg class="icon">
-            <use :xlink:href="`#i-${icon}`"></use>
+            <use :xlink:href="'#i-'+icon"></use>
          </svg>
-         <slot>按钮</slot>
+         <span class='text'>
+            <slot>按钮</slot>
+         </span>
+        
     </button>
 </template>
 <script>
     export default{
-        props:['icon']
+        props:['icon','iconPosition']
     }
 </script>
 <style lang="scss">
@@ -26,6 +29,11 @@
         &:active{
             background-color: var(--border-active-bg);
         }
+    }
+    .icon{
+        width: 3em;
+        height: 2em;
+        color:green;
     }
 </style>
 
