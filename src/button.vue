331baebb0f62dc6b392958baef9d129dfc73ef26
641290900@qq.com <641290900@qq.com>
2018-10-16 23:36:57 +0800

    优化props属性

--- src/button.vue
+++ src/button.vue
@@ -1,6 +1,6 @@
 <template>
-    <button class='v-button' :class='{}'>
-         <svg class="icon">
+    <button class='v-button' :class='{[`icon-${iconPosition}`]:true}'>
+         <svg v-if="icon" class="icon">
             <use :xlink:href="'#i-'+icon"></use>
          </svg>
          <span class='text'>
@@ -11,7 +11,17 @@
 </template>
 <script>
     export default{
-        props:['icon','iconPosition']
+        props:{
+            "icon":{},
+            "iconPosition":{
+                type: String,
+                default: "left",
+                validator: function (value) {
+                    // 这个值必须匹配下列字符串中的一个
+                    return ['left', 'right'].indexOf(value) !== -1
+                }
+            }
+        }
     }
 </script>
 <style lang="scss">
@@ -20,6 +30,24 @@
         height: var(--button-height);
         padding: 0 1em;
         border-radius: var(--border-radius);
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: middle;
+        > .icon{
+            order: 1;
+        }
+        > .text{
+            order: 2;
+        }
+        &.icon-right{
+            > .icon{
+                order: 2;
+            }
+            > .text{
+                order: 1;
+            }
+        }
         &:hover{
             background-color: var(--border-hover-bg);
         }
@@ -29,11 +57,12 @@
         &:active{
             background-color: var(--border-active-bg);
         }
-    }
-    .icon{
-        width: 3em;
-        height: 2em;
+        .icon{
+        width: 1em;
+        height: 1em;
         color:green;
     }
+    }
+    
 </style>
 
